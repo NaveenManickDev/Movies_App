450527540c8ec2139e42160392c59ccd3edffbf6
NaveenManickDev <naveen.devhub@gmail.com>
2025-07-02 08:13:53 +0530

    API Integration for getting movies

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -6,7 +6,7 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa'
 const Moviecard = ({movie}) => {
   return (
     <div className='bg-gray-800 p-4 rounded-lg shadow-md text-white relative'>
-        <img src={movie.poster} alt={movie.title} className='w-full h-80 object-contain rounded-sm' />
+        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className='w-full h-80 object-contain rounded-sm' />
         <h3 className='text-lg font-bold mt-4'>{movie.title}</h3>
         <p className='text-sm text-gray-400'>{movie.release_date}</p>
         <button className='absolute top-2 right-2 text-red-500 text-xl'>
