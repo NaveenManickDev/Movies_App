17daf881b220f92622eb0be655eb815971a5c489
NaveenManickDev <naveen.devhub@gmail.com>
2025-07-02 15:46:50 +0530

    API Integration for Pagination and search Movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,31 @@ import Moviecard from '../components/Moviecard'
 const Home = () => {
 
   const [movies, setMovies] = useState([])
-
+  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState("")
+ 
   useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=5c8e33ae58b4b69af48f3444fd46fc74")
+
+    let url = `https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=5c8e33ae58b4b69af48f3444fd46fc74`
+
+    if(search) {
+      url=`https://api.themoviedb.org/3/search/movie?query=${search}&api_key=5c8e33ae58b4b69af48f3444fd46fc74`
+    }
+
+    fetch(url)
       .then((response) => response.json())
       .then((data) => setMovies(data.results))
-  }, [])
+  }, [page, search])
   
 
   return (
     <div className='p-4 pt-16'>
       <input type="text" placeholder='Search Movies...'
       className='p-2 w-3/4 md:w-1/2 border rounded border-gray-700 bg-gray-900 
-      bg-opacity-60 *:backdrop:backdrop-blur-md text-white fixed top-16 left-1/2 transform -translate-x-1/2 z-10'/>
+      bg-opacity-60 *:backdrop:backdrop-blur-md text-white fixed top-16 left-1/2
+      transform -translate-x-1/2 z-10' onChange={(e) => {
+        setSearch(e.target.value)
+      }}/>
       <div className="movies_container grid grid-cols-1 md:grid-cols-3 
       lg:grid-cols-4 gap-4 mt-16">
         {movies.map(movie => {
@@ -28,8 +39,16 @@ const Home = () => {
         })}
       </div>
       <div className="pagination_container flex justify-between mt-5">
-        <button className='p-2 bg-gray-700 text-white rounded'>PREV</button>  
-        <button className='p-2 bg-gray-700 text-white rounded'>NEXT</button>  
+        <button
+        disabled={page==1}
+        className='p-2 bg-gray-700 text-white rounded'
+        onClick={() => {
+          setPage(prev => prev -1)
+        }}>PREV</button>  
+        <button className='p-2 bg-gray-700 text-white rounded'
+        onClick={() => {
+          setPage(prev => prev +1)
+        }}>NEXT</button>  
       </div>   
     </div>
   )
